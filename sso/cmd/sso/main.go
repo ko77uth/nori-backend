af7e118d6a92878bddd31d62a282de510b2508a4
ko77uth <izmailovmaksim99@google.com>
2026-07-01 15:05:10 +0300

    Initial commit: Nori backend with gRPC auth service

---
+++ sso/cmd/sso/main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"log/slog"
+	"nori/internal/app"
+	"nori/internal/config"
+	"nori/internal/lib/logger/handlers/slogpretty"
+	"os"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	log := setupLogger(cfg.Env)
+
+	log.Info("starting application", slog.String("env", cfg.Env))
+
+	application := app.New(log, cfg.GRPC.Port, cfg.StoragePath, cfg.TokenTTL)
+
+	application.GRPCSrv.MustRun()
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+
+	switch env {
+	case envLocal:
+		log = setupPrettySlog()
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	}
+	return log
+}
+
+func setupPrettySlog() *slog.Logger {
+	opts := slogpretty.PrettyHandlerOptions{
+		SlogOpts: &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		},
+	}
+	handler := opts.NewPrettyHandler(os.Stdout)
+	return slog.New(handler)
+}
--- sso/go.mod
+++ sso/go.mod
@@ -1,3 +1,25 @@
 module nori
 
 go 1.26.3
+
+require (
+	github.com/fatih/color v1.19.0
+	github.com/ilyakaznacheev/cleanenv v1.5.0
+	github.com/ko77uth/nori-backend/contracts v0.0.0-20260629202144-f43fd3747e01
+	golang.org/x/exp v0.0.0-20260611194520-c48552f49976
+	google.golang.org/grpc v1.82.0
+)
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/net v0.53.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/text v0.36.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20260414002931-afd174a4e478 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
